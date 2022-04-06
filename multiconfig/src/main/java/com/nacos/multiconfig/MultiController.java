5c632328c4ac9bddf0ae787e7c249f09c418f9ed
sunwer54 <1435793007@qq.com>
2022-04-06 22:14:04 +0800

    sunwer-nacos最后版本：注册中心，负载均衡，配置中心

--- multiconfig/src/main/java/com/nacos/multiconfig/MultiController.java
+++ multiconfig/src/main/java/com/nacos/multiconfig/MultiController.java
@@ -6,9 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * nacos配置中心可以用来管理不同的微服务中需要共同使用的数据
+ * 实现修改一处即修改了所有的
+ */
 @Controller
-@RefreshScope
+@RefreshScope //开启动态更新(nacos config中的数据修改了，这里会动态的更新)
 public class MultiController {
+    /**
+     * 从nacos的配置中心动态的取值
+     */
     @Value("${manager.mid}")
     private String mid;
     @Value("${manager.mname}")
--- singleconfig/src/main/java/com/nacos/config/ConfigController.java
+++ singleconfig/src/main/java/com/nacos/config/ConfigController.java
@@ -5,9 +5,16 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * nacos配置中心可以用来管理不同的微服务中需要共同使用的数据
+ * 实现修改一处即修改了所有的
+ */
 @RestController
-@RefreshScope
+@RefreshScope //开启动态更新(nacos config中的数据修改了，这里会动态的更新)
 public class ConfigController {
+    /**
+     * 从nacos的配置中心动态的取值
+     */
     @Value("${person.name}")
     private String name;
     @Value("${person.age}")
